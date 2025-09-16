e389a9faae4084c861bd27647ac3d07416f6acba
Group 5 <peterwongprasert@csu.fullerton.edu>
2025-09-15 23:10:18 -0700

    Initial Commit

---
+++ AdaptFitnessTests/AdaptFitnessTests.swift
@@ -0,0 +1,16 @@
+//
+//  AdaptFitnessTests.swift
+//  AdaptFitnessTests
+//
+//  Created by csuftitan on 9/15/25.
+//
+
+import Testing
+
+struct AdaptFitnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
