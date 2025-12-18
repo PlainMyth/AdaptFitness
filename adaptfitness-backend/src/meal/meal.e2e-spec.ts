1e4e0c758c1d9a084ad1c5d766f64100d5d2b08c
Flores33 <ivan_flores33@yahoo.com>
2025-12-17 18:40:13 -0800

    fix: correct supertest import in meal.e2e-spec.ts

--- adaptfitness-backend/src/meal/meal.e2e-spec.ts
+++ adaptfitness-backend/src/meal/meal.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from '../app.module';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Meal } from './meal.entity';
